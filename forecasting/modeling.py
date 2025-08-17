3af662e94447e0656948fc7d4cd37f57d0b97286
Sahil Khan <khan.sahil2412@gmail.com>
2025-08-18 04:43:47 +0530

    Create modeling.py

---
+++ forecasting/modeling.py
@@ -0,0 +1,17 @@
+from fbprophet import Prophet
+import pandas as pd
+
+
+def train_and_forecast(df, periods=31, freq="D"):
+    """
+    Train a Prophet model and return forecast.
+    df: DataFrame with columns ['Datetime', 'Energy_kWh']
+    """
+    data = df.rename(columns={"Datetime": "ds", "Energy_kWh": "y"})
+    model = Prophet()
+    model.fit(data)
+
+    future = model.make_future_dataframe(periods=periods, freq=freq)
+    forecast = model.predict(future)
+
+    return model, forecast
